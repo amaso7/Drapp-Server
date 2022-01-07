f7dfead4eab3525f4fc93f80fc5a6eb04a5d9f82
amaso7 <saadhindash@gmail.com>
2022-01-07 17:07:15 -0500

    fix db connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 const port = process.env.PORT || 5000
 const app = express()
 const SELECT_ALL_Pt_QUERY = 'SELECT * FROM heroku_da43e4b976c21c8.pts'
-
+import dotenv from 'dotenv'
 //db connection
 app.use(cors())
 //db connection
